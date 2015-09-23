14b69fd1f692e842d0036e8069f3c0ec626a19d1
Vladimir Golovanov <vladimirgolovanov@gmail.com>
2015-09-23 21:57:14 +0300

    Исключение для пустой таблицы bookprice

--- app/Book.php
+++ app/Book.php
@@ -18,7 +18,8 @@ class Book extends Model
         ));*/
         $books = DB::table('books')->orderBy('books.id', 'asc')->get();
         $bookprice = DB::select("SELECT `bookprice`.`book_id`, `bookprice`.`price` FROM `bookprice` WHERE `book_id` IN (SELECT `book_id` FROM `bookprice` GROUP BY `book_id` HAVING `created_at`=MAX(`created_at`))");
-        foreach($bookprice as $bp) $price[$bp->book_id] = $bp->price;
+        if(count($bookprice)) foreach($bookprice as $bp) $price[$bp->book_id] = $bp->price;
+        else $price = [];
         foreach($books as $b) if(!in_array($b->id, array_keys($price))) $price[$b->id] = "0";
         return [$books, $price];
     }
