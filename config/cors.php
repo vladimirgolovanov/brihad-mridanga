4ba44739525e085cde3e8408d439706dfe389ef3
Adbhuta Gauranga das <adbhutagaurangadas.gkg@gmail.com>
2017-12-14 15:09:49 +0300

    cors origin headers

--- config/cors.php
+++ config/cors.php
@@ -10,9 +10,9 @@ return [
      | to accept any value.
      |
      */
-    'supportsCredentials' => false,
+    'supportsCredentials' => true,
     'allowedOrigins' => explode(',', env('CORS_ORIGINS')),
-    'allowedHeaders' => ['Content-Type', 'X-Requested-With', 'Origin'],
+    'allowedHeaders' => ['Overwrite', 'Destination', 'Content-Type', 'Depth', 'User-Agent', 'Translate', 'Range', 'Content-Range', 'Timeout', 'X-File-Size', 'X-Requested-With', 'If-Modified-Since', 'X-File-Name', 'Cache-Control', 'Location', 'Lock-Token', 'If'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => [],
     'maxAge' => 0,
